cabc6afd35f05a66b8888f434e711a608d2577db
adhnowo <139139944+adhnowo@users.noreply.github.com>
2023-10-06 13:51:06 +0700

    Add files via upload

---
+++ Quiz_satu.dart
@@ -0,0 +1,16 @@
+import "dart:io";
+
+void main() {
+  //menggambungkan kalimat hallo dunia
+
+  String KalimatPertama = "hello";
+  String KalimatKedua = "dunia";
+
+  print(KalimatPertama + KalimatKedua);
+}
+
+String NamaLengkap = "Rafi Izzatul";
+String Namadepa = "Rafi";
+String NamaBelakang = "Izzatul";
+namalengkap.subsstring(5.12);
+print(print NamaDepan + print namabelakang)
